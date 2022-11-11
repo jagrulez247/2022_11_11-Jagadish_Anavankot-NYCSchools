124f644296ebb9768fc996390b507c7a8f78df2f
jagadish <jagadish@farmsteadapp.com>
2022-11-11 14:13:06 -0800

    Add project base

---
+++ app/src/main/java/com/education/nycschools/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.education.nycschools
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.education.nycschools.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
